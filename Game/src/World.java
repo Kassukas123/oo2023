be2688def25a4ad13ff56f41466da8588160ec4d
Kevin-Kaspar Einsok <114922098+Kassukas123@users.noreply.github.com>
2023-03-30 15:08:35 +0300

    Mängu alged

---
+++ Game/src/World.java
@@ -0,0 +1,47 @@
+import java.util.List;
+
+public class World {
+    int width;
+    int height;
+
+    public World(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void printMap(int worldWidth, int worldHeight,
+                         int playerXCoordinaate, int playerYCoordinaate, char playerSymbol,
+                         int dragonXCoordinaate, int dragonYCoordinaate, char dragonSymbol,
+                         int orcXCoordinaate, int orcYCoordinaate, char orcSymbol, List<Item> items){
+        // algväärtus   kuni   iga tsükkel
+        char symbol = ' '; //luuakse uus muutuja (deklareerimine), esmakordne väärtuse andmine initsialiseerimine
+        for (int y = 0; y < worldHeight; y++) {
+            System.out.println();
+            for (int x = 0; x < worldWidth; x++) {
+                if (y == 0 || y == worldHeight - 1) {
+                    symbol = '-';
+                } else if (x == 0 || x == worldWidth - 1) {
+                    symbol = '|';
+                } else {
+                    // ctrl + alt + m
+                    // right click -> refactor -> extract method
+                    for (Item i: items) {
+                        if (i.yCoordinate == y && i.xCoordinate == x) {
+                            symbol = 'I';
+                            break;
+                        }
+                    }
+                    if (playerXCoordinaate == x && playerYCoordinaate == y) {
+                        symbol = playerSymbol;
+                    } else if (dragonXCoordinaate == x && dragonYCoordinaate == y) {
+                        symbol = dragonSymbol;
+                    } else if (orcXCoordinaate == x && orcYCoordinaate == y) {
+                        symbol = orcSymbol;
+                    }
+                }
+                System.out.print(symbol);
+                symbol = ' ';
+            }
+        }
+    }
+}
