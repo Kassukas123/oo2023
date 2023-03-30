235a58ee1b8d2ff881b7bfdecf222530825ecb1b
Kevin-Kaspar Einsok <114922098+Kassukas123@users.noreply.github.com>
2023-03-30 15:45:09 +0300

    Game of the year alged

---
+++ Game/src/Dragon.java
@@ -0,0 +1,16 @@
+public class Dragon {
+    int xCoordinaate; // taisarvuline number
+    int yCoordinaate;
+    char symbol;
+
+    // constructor
+    public Dragon(int worldWidth, int worldHeight) {
+        this.xCoordinaate = getRandomCoordinaate(worldWidth);
+        this.yCoordinaate = getRandomCoordinaate(worldHeight);
+        this.symbol = 'D';
+    }
+
+    private static int getRandomCoordinaate(int worldDimension) {
+        return (int) (Math.random() * (worldDimension - 2) + 1);
+    }
+}
